f9bd64cc27675d7d76392b676f069ece67b15850
Litwiniec Maciej (CI/DAD4.5) <lim1wz@bosch.com>
2018-11-19 16:33:49 +0100

    added writing to file

--- converter.js
+++ converter.js
@@ -1,13 +1,16 @@
 const csvToJson = require("csvtojson");
 const csvFilePath = "customer-data.csv";
+const fs = require("fs");
 
 csvToJson()
     .fromFile(csvFilePath)
     .then((jsonObj) => {
         console.log(jsonObj);
-
-        //should save to file using fs
-        //error handling should be implemented
+        var json = JSON.stringify(jsonObj, null, 4);
+        fs.writeFileSync("message.json", json, 'utf8', (error) => {
+            if (error) return console.error(error)
+            console.log("Writing to file is done!");
+        });
 });
 
 
